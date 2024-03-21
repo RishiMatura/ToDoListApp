7b809d78e63076d09d39a872e614512248819286
RishiMatura <rishimatura27@gmail.com>
2024-03-21 18:09:59 +0530

    added a class for CategorySelection

---
+++ app/src/main/java/com/example/todolistapp/RecyclerViewFiles/CategoryDialog.java
@@ -0,0 +1,28 @@
+package com.example.todolistapp.RecyclerViewFiles;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class CategoryDialog {
+    private String[] categories = {"Food", "Groceries", "Work", "Personal", "Health"};
+
+    public interface OnCategorySelectedListener {
+        void onCategorySelected(String category);
+    }
+
+    public static void showCategoryDialog(Context context, String[] categories, final OnCategorySelectedListener listener) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Select Category")
+                .setItems(categories, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        String selectedCategory = categories[which];
+                        if (listener != null) {
+                            listener.onCategorySelected(selectedCategory);
+                        }
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
